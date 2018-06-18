a08bea81037a7cc974f5919b5977934cd26f1bbf
Panagiotis Migkotzidis <panagiotismigo@gmail.com>
2018-06-18 11:36:26 +0300

    commit

--- Assets/Scripts/UserIdentification.cs
+++ Assets/Scripts/UserIdentification.cs
@@ -29,27 +29,27 @@ public class UserIdentification : MonoBehaviour
         return false;
     }
     
-    public void StartSimulation()
+    public void StartSimulation(string sceneToLoad)
     {
 		goedle_sdk.GoedleAnalytics.instance.requestStrategy();
 		string user_id_raw = GameManager.instance.playerName + GameManager.instance.playerClass + GameManager.instance.playerSchoolName;
         // Creating a hashed user id, md5 hash of a string and then using a guid
         string hashed_user_id = GoedleUtils.userHash(user_id_raw);
         goedle_sdk.GoedleAnalytics.instance.setUserId(hashed_user_id);
-		StartCoroutine(getStrategy());
+		StartCoroutine(getStrategy(sceneToLoad));
 
     }
 
-	public IEnumerator getStrategy()
+	public IEnumerator getStrategy(string sceneToLoad)
 	{
-		int c = 0;
+		/*int c = 0;
 		while (goedle_sdk.GoedleAnalytics.instance.goedle_analytics.strategy == null || c < 150)
 		{
 			yield return null;
 			c++;
-		}
-		GameManager.instance.LoadLevel("Stage1");
-
+		}*/
+		GameManager.instance.LoadLevel(sceneToLoad);
+        return null;
 	}
     
 
